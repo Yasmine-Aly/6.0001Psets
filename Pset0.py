92cd2fcb7534362ff2bd28664554683306e580b8
Yasmine Ali <67972643+Yasmine-Aly@users.noreply.github.com>
2022-06-27 03:31:18 -0700

    Add files via upload

---
+++ Pset0.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+import numpy
+import math
+import pylab
+#for the log math
+x = input('Enter number x:')
+y= input('Enter number y:')
+#input returns a string; ** requires 2 numbers.
+x1=int(x)
+x2=int(y)
+print("x ** y =",x1**x2)
+print('log(x) = ',math.log(x1))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
